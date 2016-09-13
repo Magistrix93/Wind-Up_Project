c8932851c59382cd2f3f6e44322b3be082474bd2
R1ddik <r1ddik1990@hotmail.it>
2016-09-13 11:45:33 +0200

    prefab e script
    prefab per costruire il livello e script cambio scena

---
+++ Assets/Scripts/UnityCore/DoorTrigger.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class DoorTrigger : MonoBehaviour
+{
+    public GameObject key;
+    public bool puoiEntrare;
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        puoiEntrare = key.GetComponent<KeyDoorTrigger>().chiave;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if (puoiEntrare)
+                SceneManager.LoadScene("scena 3");
+
+        }
+    }
+}
---
+++ Assets/Scripts/UnityCore/KeyDoorTrigger.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyDoorTrigger : MonoBehaviour
+{
+    public bool chiave;
+
+    // Use this for initialization
+    void Start()
+    {
+        chiave = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            chiave=true;
+            //Destroy(gameObject);
+        }
+    }
+}
