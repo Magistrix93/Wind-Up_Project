09a8958fe3ae5fb502cddf7f78b43a58d2a7fe1e
Magistrix93 <simonebalbi93@libero.it>
2016-09-15 18:09:56 +0200

    Simone - Hidden Item Script Updated

--- Assets/Scripts/UnityCore/Hidden Items Behaviour/ItemHiddenBehaviour.cs
+++ Assets/Scripts/UnityCore/Hidden Items Behaviour/ItemHiddenBehaviour.cs
@@ -1,10 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class ItemHiddenBehaviour : MonoBehaviour
 {
     private Renderer rend;
     private Collider coll;
+
+    private GameObject particles;
+
+    private bool fading = false;
+    private bool check = false;
+
+    private Color color;
+    private Color startColor;
+
+    public float intensity = 0;
 
     // Use this for initialization
     void Start()
@@ -13,22 +24,47 @@ public class ItemHiddenBehaviour : MonoBehaviour
         rend.enabled = false;
         coll = GetComponent<Collider>();
         coll.isTrigger = true;
-
+        particles = transform.Find("Hidden_particles").gameObject;
+        particles.SetActive(false);
 
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (fading)
+        {
+            intensity += 100 * Time.deltaTime;
+            if (intensity >= 100)
+                intensity = 100;
+            color = startColor * intensity;
+            rend.material.SetColor("_EmissionColor", color);
+            if (intensity == 100)
+                fading = false;
 
+        }
     }
 
     void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Torch"))
-        {
-            coll.isTrigger = false;
-            rend.enabled = true;
-        }
+        if(!check)
+            if ((other.CompareTag("Torch")) && (coll.isTrigger))
+            {
+                coll.isTrigger = false;
+                rend.enabled = true;
+                startColor = rend.material.GetColor("_EmissionColor");
+                fading = true;
+                StartCoroutine(StartParticles());
+                check = true;
+            }
+    }
+
+
+
+    private IEnumerator StartParticles()
+    {
+        particles.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        particles.SetActive(false);
     }
 }
