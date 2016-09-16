b117d722e55a89ef57fa485345064afb103a6b9a
Magistrix93 <simonebalbi93@libero.it>
2016-09-16 13:15:06 +0200

    Simone - Cube Tile Script Added + Prefab Updated

---
+++ Assets/Scripts/UnityCore/GameManaging/CubeTileBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeTileBehaviour : MonoBehaviour
+{
+    private Material[] materials;
+
+    private Renderer rend;
+
+    // Use this for initialization
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        materials = rend.materials;
+        materials[0].mainTextureScale = new Vector2(transform.localScale.x, transform.localScale.y);
+        materials[1].mainTextureScale = new Vector2(transform.localScale.x, transform.localScale.z);
+        materials[2].mainTextureScale = new Vector2(transform.localScale.z, transform.localScale.y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
