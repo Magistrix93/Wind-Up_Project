e188feadc5a08dcdeed86bdaba92561133349e5f
Magistrix93 <simonebalbi93@libero.it>
2016-09-30 22:08:48 +0200

    Simone - Scott Prefab

--- Assets/Scripts/UnityCore/Character/VisorBehaviour.cs
+++ Assets/Scripts/UnityCore/Character/VisorBehaviour.cs
@@ -4,13 +4,18 @@ using System.Collections;
 public class VisorBehaviour : MonoBehaviour
 {
     private GameObject spotlight;
+    public GameObject player;
+    private Material mat;
+    private GameObject conoMesh;
     // Use this for initialization
     void Start()
     {
         spotlight = transform.Find("Spotlight").gameObject;
         GetComponent<MeshCollider>().enabled = false;
-        GetComponent<MeshRenderer>().enabled = false;
+        conoMesh = transform.Find("Mesh Renderer").gameObject;
+        conoMesh.GetComponent<MeshRenderer>().enabled = false;
         spotlight.SetActive(false);
+        mat = player.GetComponent<Renderer>().material;
     }
 
     // Update is called once per frame
@@ -22,8 +27,11 @@ public class VisorBehaviour : MonoBehaviour
     public void Active()
     {
         GetComponent<MeshCollider>().enabled = true;
-        GetComponent<MeshRenderer>().enabled = true;
+        conoMesh.GetComponent<MeshRenderer>().enabled = true;
         spotlight.SetActive(true);
+        mat.SetFloat("_MKGlowPower", 1f);
+        mat.SetFloat("_MKGlowTexStrength", 1f);
+        mat.SetFloat("_Emission", 1f);
     }
 
 }
