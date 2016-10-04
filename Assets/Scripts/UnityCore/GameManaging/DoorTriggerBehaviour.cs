85a52a4795ed2cf2896e764a7e12d3621c5ffc12
Magistrix93 <simonebalbi93@libero.it>
2016-10-04 15:53:03 +0200

    Simone - Scena Salvio 5

--- Assets/Scripts/UnityCore/GameManaging/DoorTriggerBehaviour.cs
+++ Assets/Scripts/UnityCore/GameManaging/DoorTriggerBehaviour.cs
@@ -7,6 +7,10 @@ public class DoorTriggerBehaviour : MonoBehaviour
     private GameObject key;
     private KeyDoorTriggerBehaviour door;
     private bool accessON;
+    private Texture emissSbloccata;
+    private Renderer rend;
+    private int currentScene;
+    private int nextScene;
 
     // Use this for initialization
     void Start()
@@ -14,6 +18,10 @@ public class DoorTriggerBehaviour : MonoBehaviour
         key = GameObject.FindGameObjectWithTag("Key");
         accessON = false;
         door = key.GetComponent<KeyDoorTriggerBehaviour>();
+        emissSbloccata = Resources.Load<Texture>("emiss_sbloccata");
+        rend = GetComponent<Renderer>();
+        currentScene = SceneManager.GetActiveScene().buildIndex;
+        nextScene = currentScene + 1;
     }
 
     // Update is called once per frame
@@ -28,7 +36,8 @@ public class DoorTriggerBehaviour : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             if (accessON)
-                SceneManager.LoadScene("scena 3");
+                rend.material.SetTexture("_MKGlowTex", emissSbloccata);
+                SceneManager.LoadScene(nextScene);
 
         }
     }
